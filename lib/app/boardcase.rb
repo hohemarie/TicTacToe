3ce0a51beded9d72fce51a45d95deb5db4cb44aa
ArthurC2Pouce <agencecoupdepouce@gmail.com>
2019-01-24 10:31:07 +0100

    changes on Boardcase, board and show

--- lib/app/boardcase.rb
+++ lib/app/boardcase.rb
@@ -1,10 +1,16 @@
-
 class BoardCase
+	attr_accessor :value :board_case
   #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
 
   
-  def initialize
+  def initialize(board_case)
+  	@board_case = board_case
+  	@value = " "
     #TO DO : doit régler sa valeur, ainsi que son numéro de case
+  end
+
+  def change_value(value)
+  	@value = value
   end
   
 end
